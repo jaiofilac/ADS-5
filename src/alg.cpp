1127a3dd542dfa09fd8673757cc7b3dfc308841d
Андрей Жаворонков <96244220+jaiofilac@users.noreply.github.com>
2022-04-03 23:37:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,99 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+using namespace std;
+
+int priority(char symbol) {
+    switch (symbol) {
+        case '(': return 0;
+        case ')': return 1;
+        case '+': return 2;
+        case '-': return 2;
+        case '*': return 3;
+        case '/': return 3;
+        default: return -1;
+    }
 }
 
-int eval(std::string pref) {
-  // добавьте код
-  return 0;
+string infx2pstfx(string inf) {
+    string result = "";
+    TStack <char, 100> symbolStack;
+    for (int i = 0; i < inf.length(); i++) {
+        if (priority(inf[i]) == -1) {
+            result += inf[i];
+        } else if (priority(inf[i]) > 1 && priority(inf[i]) < 4) {
+            if (symbolStack.isEmpty() || priority(symbolStack.get()) == 0 || (priority(inf[i]) > priority(symbolStack.get()))) {
+                symbolStack.push(inf[i]);
+                result += " ";
+            } else if (priority(inf[i]) <= priority(symbolStack.get())) {
+                result += " ";
+                result += symbolStack.get();
+                symbolStack.pop();
+                result += " ";
+                while ((priority(inf[i]) <= priority(symbolStack.get())
+                    || priority(symbolStack.get()) != 0) && !symbolStack.isEmpty()) {
+                    result += " ";
+                    result += symbolStack.get();
+                    symbolStack.pop();
+                    result += " ";
+                }
+                symbolStack.push(inf[i]);
+            }
+        } else if (priority(inf[i]) == 0) {
+            symbolStack.push(inf[i]);
+        } else if (priority(inf[i]) == 1) {
+            while (priority(symbolStack.get()) != 0) {
+                result += " ";
+                result += symbolStack.get();
+                symbolStack.pop();
+            }
+            symbolStack.pop();
+        }
+    }
+    while (!symbolStack.isEmpty()) {
+        result += " ";
+        result += symbolStack.get();
+        symbolStack.pop();
+    }
+    return result;
+}
+
+int eval(string pref) {
+    TStack<int, 100> intStack;
+    string numberString = "";
+    bool isNum = false;
+
+    for (int i = 0; i < pref.length(); i++) {
+        if (pref[i] <= '9' && pref[i] >= '0') {
+            isNum = true;
+            numberString += pref[i];
+        }
+        else {
+            if (isNum) {
+                intStack.push(stoi(numberString));
+                isNum = false;
+                numberString = "";
+            } else if (pref[i] != ' ') {
+                int a = intStack.get();
+                intStack.pop();
+                int b = intStack.get();
+                intStack.pop();
+                switch (pref[i]) {
+                case '+':
+                    intStack.push(b + a);
+                    break;
+                case '-':
+                    intStack.push(b - a);
+                    break;
+                case '*':
+                    intStack.push(b * a);
+                    break;
+                case '/':
+                    intStack.push(b / a);
+                    break;
+                }
+            }
+        }
+    }
+    return intStack.get();
 }
